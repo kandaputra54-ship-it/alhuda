0feab892485719c4498ccd9df2fe7855c2366645
IryssLabs <kandaputrah@gmail.com>
2026-05-29 10:59:42 +0700

    up

--- src/lib/weeklySchedule.ts
+++ src/lib/weeklySchedule.ts
@@ -98,7 +98,7 @@ export const weeklySchedule: Record<string, DaySchedule> = {
       muadzin: "Azan Salaeh",
     },
     jumat: {
-      khatib: "Warta",
+      khatib: "Ishaq Ismail",
       imam: "M. Syamsudin",
       muadzin: "Muadzin Jumat",
     },
