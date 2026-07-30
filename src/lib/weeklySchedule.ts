812f989165b71e70835eae9c46471cc9a8b5d893
IryssLabs <kandaputrah@gmail.com>
2026-07-30 16:23:27 +0700

    update

--- src/lib/weeklySchedule.ts
+++ src/lib/weeklySchedule.ts
@@ -61,7 +61,7 @@ export const weeklySchedule: Record<string, DaySchedule> = {
   },
   Jumat: {
     subuh: { utama: "Syakir Abdullah", badal: "Tedi Koswara", muadzin: "Azan Salaeh", },
-    jumat: { khatib: "Sudirman Tamim", imam: "Syamsudin", muadzin: "Muadzin Jumat", },
+    jumat: { khatib: "M.Syamsudin", imam: "Syamsudin", muadzin: "Muadzin Jumat", },
     ashar: { utama: "Tedi Koswara ", badal: "Azan Saleh", muadzin: "Satria" },
     maghrib: { utama: "Tedi Koswara", badal: "Kanda Putra", muadzin: "Azan Saleh", },
     isya: { utama: "Galuh Yogaswara", badal: "Kanda Putra", muadzin: "Azan Saleh" },
